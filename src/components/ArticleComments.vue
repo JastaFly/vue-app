3482cc97df7e1e26d780316ca7cde5c5ee9beec3
Влад Мелёшкин <zumawuu@gmail.com>
2026-05-06 22:50:29 +0300

    Refactor delete comment button

--- src/components/ArticleComments.vue
+++ src/components/ArticleComments.vue
@@ -5,12 +5,17 @@ import {ref} from "vue";
 import {storeToRefs} from "pinia";
 import {useCommentsStore} from "@/stores/commentsStore";
 import {useArticleStore} from "@/stores/articleStore";
+import {useAuthStore} from "@/stores/authStore";
 
 const commentsStore = useCommentsStore()
 const articleStore = useArticleStore()
+const authStore = useAuthStore()
 const {comments} = storeToRefs(commentsStore)
 const {article} = storeToRefs(articleStore)
+const {currentUser} = storeToRefs(authStore)
 let commentTxt = ref('')
+
+console.log(currentUser.value)
 </script>
 
 <template>
@@ -30,7 +35,7 @@ let commentTxt = ref('')
           <span class="comment__name">{{comment.author.username}}</span>
           <div class="comment__date">{{formatDate(comment.createdAt)}}</div>
         </div>
-        <svg @click="commentsStore.deleteComment(article.slug, comment.id)" class="comment__delete" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m20.015 6.506h-16v14.423c0 .591.448 1.071 1 1.071h14c.552 0 1-.48 1-1.071 0-3.905 0-14.423 0-14.423zm-5.75 2.494c.414 0 .75.336.75.75v8.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-8.5c0-.414.336-.75.75-.75zm-4.5 0c.414 0 .75.336.75.75v8.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-8.5c0-.414.336-.75.75-.75zm-.75-5v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-16.507c-.413 0-.747-.335-.747-.747s.334-.747.747-.747zm4.5 0v-.5h-3v.5z" fill-rule="nonzero"/></svg>
+        <svg v-if="currentUser.username === comment.author.username" @click="commentsStore.deleteComment(article.slug, comment.id)" class="comment__delete" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m20.015 6.506h-16v14.423c0 .591.448 1.071 1 1.071h14c.552 0 1-.48 1-1.071 0-3.905 0-14.423 0-14.423zm-5.75 2.494c.414 0 .75.336.75.75v8.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-8.5c0-.414.336-.75.75-.75zm-4.5 0c.414 0 .75.336.75.75v8.5c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-8.5c0-.414.336-.75.75-.75zm-.75-5v-1c0-.535.474-1 1-1h4c.526 0 1 .465 1 1v1h5.254c.412 0 .746.335.746.747s-.334.747-.746.747h-16.507c-.413 0-.747-.335-.747-.747s.334-.747.747-.747zm4.5 0v-.5h-3v.5z" fill-rule="nonzero"/></svg>
       </div>
     </div>
   </div>
